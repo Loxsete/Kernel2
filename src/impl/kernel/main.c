6c28d2d26fffda105b2cf4b34b0091c5be6c4a42
Loxsete <gduck3029@gmail.com>
2025-01-10 14:52:13 +0200

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,5 +1,11 @@
 #include "kernel.h"
 
+void __stack_chk_fail(void) {
+    // Можно вывести сообщение об ошибке или просто выйти
+    while(1);
+}
+
+
 void kernel_main(void) {
     clear_screen();
     print("ASINC Startinc \n");
