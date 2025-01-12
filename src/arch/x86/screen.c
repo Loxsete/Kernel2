b5951f87769c08b2063b02c3e9285ff2d5cca666
predefine <predefine@yandex.ru>
2025-01-12 17:06:03 +0700

    x86: screen: implement scrolling

--- src/arch/x86/screen.c
+++ src/arch/x86/screen.c
@@ -1,6 +1,7 @@
 #include <arch/x86/screen.h>
 #include <hal/stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 static uint16_t* screen_buf = (uint16_t*)0xb8000;
 
@@ -15,10 +16,14 @@ void x86_clear_screen(void) {
 }
 
 void x86_putc(const char ch) {
-    if(screen_pos > (NUM_COLS * NUM_ROWS + (ch=='\n')))
-        screen_pos -= NUM_COLS; // TODO: scroll up
+    if(screen_pos >= ((NUM_ROWS - (ch=='\n')) * NUM_COLS)){
+        memcpy(screen_buf, screen_buf + NUM_COLS, (screen_pos - NUM_COLS)*2);
+        screen_pos -= NUM_COLS - (screen_pos % NUM_COLS);
+        for(int i = 0; i < NUM_COLS; i++)
+            screen_buf[screen_pos + i] = 0;
+    }
     if(ch == '\n'){
-        screen_pos += NUM_COLS - (screen_pos % NUM_COLS) ;
+        screen_pos += NUM_COLS - (screen_pos % NUM_COLS);
         return;
     }
     screen_buf[screen_pos++] = (0x0f << 8) | ch;
